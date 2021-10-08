ec34144ba9357256d34d75e52457b2e4b9a3a8d5
adijdv <91335766+adijdv@users.noreply.github.com>
2021-10-08 21:11:35 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,3 +3,18 @@ document.querySelectorAll('article').forEach((e) => {
         location.assign('article.html')
     })    
 })
+
+let lightTheme = "styles/light.css";
+let darkTheme = "styles/dark.css";
+
+function changeTheme() {
+  let darkMode = document.getElementById("dark-mode");
+  let theme = document.getElementById("theme");
+  if (theme.getAttribute("href") == lightTheme) {
+    theme.href = darkTheme;
+    darkMode.innerHTML = "Light Mode 🌞";
+  } else {
+    theme.href = lightTheme;
+    darkMode.innerHTML = "Dark Mode 🌙";
+  }
+}
