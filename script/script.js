8adf106689caa4a0ac38a0654226b620d2733f51
Soumyadeep Basu <85547399+Geralt58@users.noreply.github.com>
2022-01-07 20:29:37 +0530

    Revert "added dark mode feature for better readability of users"

--- script/script.js
+++ script/script.js
@@ -3,18 +3,3 @@ document.querySelectorAll('article').forEach((e) => {
         location.assign('article.html')
     })    
 })
-
-let lightTheme = "css/light.css";
-let darkTheme = "css/dark.css";
-
-function changeTheme() {
-  let darkMode = document.getElementById("dark-mode");
-  let theme = document.getElementById("theme");
-  if (theme.getAttribute("href") == lightTheme) {
-    theme.href = darkTheme;
-    darkMode.innerHTML = "Light Mode 🌞";
-  } else {
-    theme.href = lightTheme;
-    darkMode.innerHTML = "Dark Mode 🌙";
-  }
-}
